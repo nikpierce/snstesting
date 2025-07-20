cb30037356f39306e8e465d037c3f5115371b4c4
Alan Harris <email@alanoharris.com>
2025-07-20 16:04:41 +0100

    standardise style within page

--- toolkit/settings_staging.py
+++ toolkit/settings_staging.py
@@ -6,8 +6,8 @@ from toolkit.settings_common import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_PASSWORD"),
         'HOST': os.environ.get("DB_HOST"), 
         'PORT': os.environ.get("DB_PORT"),
