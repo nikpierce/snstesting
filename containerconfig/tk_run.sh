dedcedf60adb59f2aa0f2a93392dc9d470b57eb0
nikpierce <nikki.p@tuta.io>
2025-11-11 20:35:52 +0000

    Update tk_run.sh

--- containerconfig/tk_run.sh
+++ containerconfig/tk_run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+#Exit immediately on error, treating unset variables as error.
 set -eu
 
+#Empty string as argument if none given - may be redundant?
 COMMAND=${1:-}
 
-echo "Running as: $(id)"
-
+# Need to decide which DB to use rather than this else nonesense.
 if [[ -v DB_HOST && -v DB_PORT && -n $DB_HOST && -n $DB_PORT ]] ; then
   if ! wait-for-it $DB_HOST:$DB_PORT --timeout=360 ; then
     echo "Database host not available"
@@ -19,6 +20,7 @@ case "$COMMAND" in
     gunicorn)
         echo "Running database migrations"
         /venv/bin/python3 /site/manage.py migrate
+        #Temp secret key used, not great?
         SECRET_KEY="X" /venv/bin/python3 /site/manage.py collectstatic --noinput --settings=toolkit.settings
         exec /venv/bin/gunicorn wsgi --bind 0.0.0.0:8000 --chdir /site
         ;;
